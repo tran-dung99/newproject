303585cda864d694d6c425779fa19f4ffb1c4127
Dung <trandungxx2712@gmail.com>
2021-11-10 08:49:19 +0700

    commit

---
+++ data.php
@@ -0,0 +1,8 @@
+<?php
+$array = [
+    ["name" => "dung","age"=>18,"address"=>"nghe an"],
+    ["name" => "nam","age"=>15,"address"=>"ha noi"],
+    ["name" => "lan","age"=>20,"address"=>"phu tho"],
+    ["name" => "luc","age"=>30,"address"=>"dubai"],
+];
+?>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+include_once "data.php";
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<table>
+    <tr>
+        <th>name</th>
+        <th>age</th>
+        <th>address</th>
+    </tr>
+    <?php foreach($array as $key => $value):?>
+    <tr>
+        <td><?php echo $value["name"] ?></td>
+    </tr>
+    <?php endforeach;?>
+</table>
+</body>
+</html>
+
+
